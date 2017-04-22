add4194fd8fe1fba8a68e1fe415a2ded70685eee
Maria Susana Moline Venanzi <rafadiver@gmail.com>
2017-04-22 13:11:09 -0500

    Initial commit

--- CanadianTaxCalculator Tutorial/ViewController.swift
+++ CanadianTaxCalculator Tutorial/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var lblTitleLabel: UILabel!
+    @IBOutlet weak var txtInputCostLabel: UITextField!
+    @IBOutlet weak var pkrPromptPickerLabel: UIPickerView!
+    @IBOutlet weak var btnCalculateButtonLabel: UIButton!
+    @IBOutlet weak var lblTotalCostLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.pkrPromptPickerLabel.delegate = self
+        self.pkrPromptPickerLabel.dataSource = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func pressCalculateCostACTION(_ sender: UIButton) {
+    }
+    
+ 
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        //return listOfProvices.count
+    }
+    
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        <#code#>
+        //return listOfProvices.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        <#code#>
+    }
+    
+    
 }
 
